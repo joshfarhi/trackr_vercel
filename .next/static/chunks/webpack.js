ec4fe0a2de8f9e5192f8008c97218457ef7e3f13
joshfarhi <joshuafarhi@protonmail.com>
2024-08-23 13:54:55 -0400

    deleted InventoryTable that was not used wip

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5c29a796229b615d"; }
+/******/ 		__webpack_require__.h = function() { return "10bfe142c9fb3969"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
