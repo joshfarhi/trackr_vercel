a973c1fa1622190592343b5b32ddc816882d5cd7
joshfarhi <joshuafarhi@protonmail.com>
2024-08-23 14:12:18 -0400

    deleted stats card

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3c5fd56ab03d0287"; }
+/******/ 		__webpack_require__.h = function() { return "a05792e57a8be9f1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
