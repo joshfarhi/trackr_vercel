b16de730cfca3855443d0afbb01a1d9b96fd65c2
joshfarhi <joshuafarhi@protonmail.com>
2024-09-20 15:30:54 -0400

    next and previous buttons removed

--- app/(dashboard)/inventory/_components/InventoryTable.tsx
+++ app/(dashboard)/inventory/_components/InventoryTable.tsx
@@ -342,24 +342,7 @@ const [pagination, setPagination] = useState({
             </TableBody>
           </Table>
         </div>
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
-        </div>
+        
       </SkeletonWrapper>
     </div>
   );
