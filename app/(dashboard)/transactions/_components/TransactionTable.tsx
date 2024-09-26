02d4d36d34bccfe10d3726cdf10cc5fc1f1ead8d
joshfarhi <joshuafarhi@protonmail.com>
2024-09-26 17:52:46 -0400

    increased page size to 2000 in transaction table (infinite scroll finesse)

--- app/(dashboard)/transactions/_components/TransactionTable.tsx
+++ app/(dashboard)/transactions/_components/TransactionTable.tsx
@@ -218,6 +218,11 @@ function TransactionTable({ from, to }: Props) {
   const [sorting, setSorting] = useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
+const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 5000,
+  });
+
   const history = useQuery<GetTransactionHistoryResponseType>({
     queryKey: ["transactions", "history", from, to],
     queryFn: () =>
@@ -455,24 +460,6 @@ function TransactionTable({ from, to }: Props) {
               )}
             </TableBody>
           </Table>
-        </div>
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
         </div>
       </SkeletonWrapper>
     </div>
